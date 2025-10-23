89f987a4269d3707f247c5c04dd14b0d74313726
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-10-22 19:57:33 -0500

    instalacion limpia de laravel a travez de composer, personalizacion de idioma, modelo de user con el role, instalacoin de laravel breeze, instalacion de filament php v4 y personalizacion del AdminPanelProvider, entre otros, menu en barra superior, tambien color, enlaces, y que se puedan registrar y recuperar contraseña

---
+++ app/Models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'role',
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'role' => 'string',
+        ];
+    }
+
+    public function scopeIsAdmin($query)
+    {
+        return $query->where('role', Role::admin->value);
+    }
+
+    public function scopeIsUser($query)
+    {
+        return $query->where('role', Role::user->value);
+    }
+}
---
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->colors([
+                'primary' => Color::Indigo,
+            ])
+            /*  */
+            ->passwordReset()
+            ->registration()
+            ->font('Poppins')
+            ->favicon(asset('assets/img/logo.png'))
+            ->darkMode(true)
+            ->sidebarCollapsibleOnDesktop()
+            // ->brandName(__(config('app.name', 'Basic Expense Tracking')))
+            ->brandLogo(asset('assets/img/logo.png'))
+            ->brandLogoHeight("2.5rem")
+            ->topNavigation()
+            // links de navegación en el menú
+            ->navigationItems([
+                \Filament\Navigation\NavigationItem::make('WhatsApp')
+                    ->url('https://wa.me/573052850514', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-chat-bubble-bottom-center-text')
+                    ->group(__('External Links'))
+                    ->visible(function (): bool {
+                       return \Illuminate\Support\Facades\Auth::check();
+                    })
+                    ->sort(99),
+            ])
+
+            ->userMenuItems([ //para el menu de usuario en la parte superior derecha
+                \Filament\Actions\Action::make('profile')
+                    ->label(__('Profile'))
+                    ->url('/profile')
+                    ->icon('heroicon-o-cog-6-tooth')
+            ])
+            /*  */
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
+            ->widgets([
+                AccountWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
